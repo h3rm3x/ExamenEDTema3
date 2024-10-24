fe8bec6cd663a3eebe98d564a579cfbc6a844978
h3rm3x <alanrabinerson@gmail.com>
2024-10-24 18:04:42 +0200

    rama comentarios eliminada

--- funcionsuma.java
+++ funcionsuma.java
@@ -1,15 +1,26 @@
 import java.util.Scanner;
 
+/**
+ * programa suma, para calcular la suma de dos variables entradas por teclado
+ * @version v1.0
+ * @author Miquel
+ * @see Para mas informacion ver <a href="https://google.com">Google</a>
+ */
 
 
 public class FuncionSuma {
-
-
-
+    /**
+     * Variable para guardar el primer sumando
+     */
     private static int primerSumando;
-
+    /**
+     * variable para gurdar el segundo sumando
+     */
     private static int segundoSumando;
 
+    /**
+     * funcion principal del programa
+     */
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
@@ -26,6 +37,12 @@ public class FuncionSuma {
 
     }
 
+    /**
+     * Funcion que realiza la suma de los dos operadores
+     * @param sumando1- primer parametro de la operacion suma
+     * @param sumando2- segundo parametro de la operacion suma
+     * @return Devuelve el resultado de sumar los dos sumandos
+     */
 
 
     private static int suma (int sumando1, int sumando2) {
